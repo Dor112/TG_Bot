720661418dcadc4a966da391c705de8984c8f6bc
Dor1 <itsuka.fortnite2020@gmail.com>
2025-11-29 20:47:16 +0300

    changed the wheather API

--- wheather.py
+++ wheather.py
@@ -1,10 +1,17 @@
 import requests
+import datetime
+import math
+
 
 async def get_wheather():
-
-    url = f'https://wttr.in/{"Moscow"}?format=%c+%C+%t+%h+%w+%p'
-
-    response = requests.get(url)
-    global output
-    output = "Погода сегодня:" + response.text
-    return output
+    date = datetime.datetime.today()
+    formated_date = date.strftime("%d.%m.%Y %H:%M")
+    res = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Moscow,ru&APPID=08f1d6a9d1833292f9d102f34ae01b3b')
+    data = res.json()
+    date_out = 'Сейчас: ' +  str(formated_date)
+    state = "Состояние: " +  str(data['weather'][0]['description'])
+    temp = "Температура: " + str(math.floor(data['main']['temp']-273.50))
+    min_temp = "Минимальная температура: " + str(math.floor(data['main']['temp_min']-273.50))
+    max_temp = "Максимальная температура: " + str(math.floor(data['main']['temp_max']-273.50))
+    str_output = date_out +"       "+ state +"          "+ temp +"          "+  min_temp +"          "+ max_temp
+    return str_output
